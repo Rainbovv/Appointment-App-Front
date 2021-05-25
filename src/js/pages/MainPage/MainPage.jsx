d55ce3e81f33b328eb169af667dd6609ea8f0526
Igor <filipenko-igor@mail.ru>
2021-05-25 12:30:26 +0300

    config: removed rule for one line

--- src/js/pages/MainPage/MainPage.jsx
+++ src/js/pages/MainPage/MainPage.jsx
@@ -3,6 +3,7 @@ import {withRouter} from "react-router-dom";
 
 import {connect} from "react-redux";
 import {
+	Button,
 	Header,
 	Segment
 } from "semantic-ui-react";
@@ -11,10 +12,17 @@ import {
 class MainPage extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {}
+		this.state = {
+			myState: true,
+			counter: 1
+		}
 	}
 
 	render() {
+		const {
+			myState
+		} = this.state;
+
 		return (
 			<Fragment>
 				<Segment>
@@ -24,6 +32,9 @@ class MainPage extends Component {
 					<Header as="h4">
 						Hello from Main Page!
 					</Header>
+					{myState ? <Button primary>Primary</Button> : <Button primary>Secondary</Button> }
+					<Button
+						primary>Primary</Button>
 				</Segment>
 			</Fragment>
 		)
--- src/js/services/HttpService.js
+++ src/js/services/HttpService.js
@@ -1,5 +1,3 @@
-
-
 const HEADERS = {
 	"Content-Type": "application/json",
 	"Accept": "application/json",
