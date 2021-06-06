b361ffe0ae6cac83c85e173f0d4b5480b2af555a
Igor <filipenko-igor@mail.ru>
2021-06-06 19:16:22 +0300

    feature1/admin-module: footer for project

---
+++ src/js/common/Footer/Footer.tsx
@@ -0,0 +1,48 @@
+import React, {Fragment} from "react";
+
+import {
+    Button,
+    Container,
+    Divider,
+    Grid,
+    Header,
+    Item,
+    List,
+    Segment,
+} from "semantic-ui-react";
+
+import "./footer-style.css";
+
+type Props = {}
+
+const Footer: React.FunctionComponent<Props> = (props) => {
+    return (
+        <div className="footer-container">
+            <Segment inverted vertical style={{padding: "5em 0em", backgroundColor: "#25283e"}}>
+                <Container>
+                    <Grid divided inverted stackable>
+                        <Grid.Row>
+                            <Grid.Column width={3}>
+                                <Header inverted as='h4' content='About'/>
+                                <List link inverted>
+                                    <List.Item>Sitemap</List.Item>
+                                    <List.Item>Contact Us</List.Item>
+                                </List>
+                            </Grid.Column>
+                            <Grid.Column width={7}>
+                                <Header as='h4' inverted>
+                                    All rights reserved
+                                </Header>
+                                <p>
+                                    This site and service is powered by Stefanini EMEA
+                                </p>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Container>
+            </Segment>
+        </div>
+    )
+};
+
+export default Footer;
